c24d3d2ad57c361ab5c4a7f1fe1915daf2bc7811
Odizinne <flora.aubry@proton.me>
2024-07-11 15:08:19 +0200

    repo restruct

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,21 @@
+import os
 from cx_Freeze import setup, Executable
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 build_dir = "build/makeSense"
+
 base = "Win32GUI"
-icon = "icons/icon.ico"
+
+icon = os.path.join(script_dir, "icons/icon.ico")
+
 zip_include_packages = ['PyQt6', 'winshell', 'dualsense-controller', 'pyautogui']
-include_files = ['icons/', 'dependencies/hidapi/hidapi.dll', 'dependencies/hidapi/hidapi.lib']
+
+include_files = [
+    os.path.join(script_dir, 'icons/'), 
+    os.path.join(script_dir, 'dependencies/hidapi/hidapi.dll'), 
+    os.path.join(script_dir, 'dependencies/hidapi/hidapi.lib')
+]
 
 build_exe_options = {
     "include_files": include_files,
@@ -14,7 +25,7 @@ build_exe_options = {
 }
 
 executables = [
-    Executable('makesense.py', base=base, icon=icon, target_name="makeSense.exe")
+    Executable(os.path.join(script_dir, 'makesense.py'), base=base, icon=icon, target_name="makeSense.exe")
 ]
 
 setup(
