eea0fbf0c8f217b4726e133fc4ed7fa9890bb239
Odizinne <flora.aubry@proton.me>
2024-07-16 09:54:44 +0200

    Added install command

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,10 @@
 import os
 from cx_Freeze import setup, Executable
+from setuptools.command.install import install as _install
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
-
 build_dir = "build/makeSense"
-
-base = "Win32GUI"
-
-icon = os.path.join(src_dir, "icons/icon.ico")
+install_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'programs', 'makeSense')
 
 zip_include_packages = ['PyQt6', 'winshell', 'dualsense-controller', 'pyautogui']
 
@@ -25,12 +22,26 @@ build_exe_options = {
 }
 
 executables = [
-    Executable(os.path.join(src_dir, 'makesense.py'), base=base, icon=icon, target_name="makeSense.exe")
+    Executable(os.path.join(src_dir, 'makesense.py'), base="Win32GUI", icon=os.path.join(src_dir, "icons/icon.ico"), target_name="makeSense.exe")
 ]
+
+class InstallCommand(_install):
+    def run(self):
+        if not os.path.exists(install_dir):
+            os.makedirs(install_dir)
+        if not os.path.exists(build_dir):
+            print("##################################################")
+            print("# Nothing to install.                            #")
+            print("# Please build the project first.                #")
+            print("##################################################")
+            return
+        self.copy_tree(build_dir, install_dir)
+        print(f"Executable installed to {install_dir}")
 
 setup(
     name='makeSense',
     version='1.0',
     options={"build_exe": build_exe_options},
-    executables=executables
+    executables=executables,
+    cmdclass={'install': InstallCommand}
 )
