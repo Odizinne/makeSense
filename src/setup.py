e6c3d34e859b0edb70327ae2117a89f1b1c4466e
Odizinne <flora.aubry@proton.me>
2024-07-16 07:45:27 +0200

    added quick vscode build task

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,20 @@
 import os
 from cx_Freeze import setup, Executable
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.dirname(os.path.abspath(__file__))
 
 build_dir = "build/makeSense"
 
 base = "Win32GUI"
 
-icon = os.path.join(script_dir, "icons/icon.ico")
+icon = os.path.join(src_dir, "icons/icon.ico")
 
 zip_include_packages = ['PyQt6', 'winshell', 'dualsense-controller', 'pyautogui']
 
 include_files = [
-    os.path.join(script_dir, 'icons/'), 
-    os.path.join(script_dir, 'dependencies/hidapi/hidapi.dll'), 
-    os.path.join(script_dir, 'dependencies/hidapi/hidapi.lib')
+    os.path.join(src_dir, 'icons/'), 
+    os.path.join(src_dir, 'dependencies/hidapi/hidapi.dll'), 
+    os.path.join(src_dir, 'dependencies/hidapi/hidapi.lib')
 ]
 
 build_exe_options = {
@@ -25,7 +25,7 @@ build_exe_options = {
 }
 
 executables = [
-    Executable(os.path.join(script_dir, 'makesense.py'), base=base, icon=icon, target_name="makeSense.exe")
+    Executable(os.path.join(src_dir, 'makesense.py'), base=base, icon=icon, target_name="makeSense.exe")
 ]
 
 setup(
