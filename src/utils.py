eb30ba32f8703d7fc4d217334c4a9207ec779cfe
Odizinne <flora.aubry@proton.me>
2024-08-05 11:08:32 +0200

    fixed windows 10 detection

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,6 @@ import platform
 
 
 def is_windows_10():
-    os_version = platform.version()
+    os_release = platform.release()
     os_name = platform.system()
-    return os_name == "Windows" and os_version.startswith("10")
+    return os_name == "Windows" and os_release == "10"
